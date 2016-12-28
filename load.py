8fd2b7ec59143bf0020dfc5ec5f0f80928d6c23a
root <yelongclass@163.com>
2016-12-28 21:28:28 +1030

    Load the csv dataset

--- load.py
+++ load.py
@@ -1,26 +1,37 @@
 ﻿# encoding:utf-8
 # Python2 兼容
-from __future__ import print_function, division
-import tesnsorflow as tf
+#from __future__ import print_function, division
+#import tensorflow as tf
 #from scipy.io import loadmat as load
 #import matplotlib.pyplot as plt
-#import numpy as np
 import csv
+from itertools import islice
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
 
-def file_len(fname):
-	with open(fname) as f:
-		for i, l in enumerater(f):
-			pass
-	return i + 1
+#Read dataset
+in_file = 'data/forest_ag_int_1988_2014.csv'
+with open(in_file,'rb') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#Print  whole row from begin
+	for i in range(2):
+		print spamreader.next()
 
-filename = "data/forest_ag_int_1988_2014.csv"
+#There are total 1376867 row in the dataset.
+#	row_count = sum(1 for row in spamreader)  # fileObject is your csv.reader
+#	print  ('There are total %s row in the dataset.' % row_count)
 
-#setup text reader
-file_length = file_len(filename)
-	
-ifile = open('data/forest_ag_int_1988_2014.csv','rb')
-reader = csv.reader(ifile)
 
+
+
+
+#outcomes = reader['f1988tr2']
+#data = reader.drop('f1988tr2', axis = 1)
+#print(data.head())
+
+#Print the whole dataset
+'''
 rownum = 0
 for row in reader:
     # Save header row.
@@ -29,9 +40,8 @@ for row in reader:
     else:
         colnum = 0
         for col in row:
-            print '%-8s: %s' % (header[colnum], col)
+            print ('%-8s: %s' % (header[colnum], col))
             colnum += 1
-            
     rownum += 1
-
 ifile.close()
+'''
